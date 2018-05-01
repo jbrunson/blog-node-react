7f4a121efd11569f047da235b9a8654a87a9ca8f
Jeff Brunson <jeff.a.brunson@gmail.com>
2018-04-30 21:51:36 -0600

    image uploader

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://jeff:password1@ds237979.mlab.com:37979/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: '<S3_ACCESS_KEY>',
+  secretAccessKey: '<S3_SECRET_KEY>'
 };
